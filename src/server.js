f3f9089f263bde4c22355a0f410ab9dd599542bb
ZUNNN31 <tranducdung310503@gmail.com>
2026-04-10 20:01:56 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from './config/connectDB'
+
 require('dotenv').config();
 
 let app = express();
@@ -11,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
 let port = process.env.PORT || 8088;
 
 app.listen(port, () => {
